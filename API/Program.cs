c9f60b03141a4254d358acc95222cc8a11fc9206
yash-2357 <mrgohil2357@gmail.com>
2025-02-16 22:09:45 +0530

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,30 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<StoreContext>(opt => {
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build(); //things before this line are services and things after this line are middlewares
+
+// Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.MapOpenApi();
+// }
+
+// app.UseHttpsRedirection();
+
+// app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
